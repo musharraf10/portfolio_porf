5fff0cb639e488aa45824420565db17f5015f314
Shaik Musharaf <105067219+musharraf10@users.noreply.github.com>
2026-07-23 00:58:57 +0530

    Add LinkedIn badge for Shaik Musharaf

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -447,6 +447,9 @@ export function Contact() {
               />
             </div>
 
+            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="skmusharaf01" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/skmusharaf01?trk=profile-badge">Shaik Musharaf</a></div>
+              
+
             {/* bottom content */}
             <div
               className="
